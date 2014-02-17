bd632d744234494753b3775b2ba897cf426934b0
Chris Gill <chrisfgill@gmail.com>
2014-02-17 10:15:10 -0500

    changes to core, implemented websocket client, fixed bug

--- README.md
+++ README.md
@@ -2,32 +2,32 @@
 
 My effort to combine both websockets and regular tcp sockets under the same roof, and to homogenize the processes and handling for both client and server with similar semantics.
 
+Is both a regular tcp socket server as well as a websocket server, is able to serve both simultaneously.
+Can either be a websocket client or regular tcp socket client.
+
 ## Installation
 
-clone this repo, run lein repl.
+For now clone this repo, run lein repl.
 
 ## Usage
 
 ```clojure
-(ns gulfstream.client
-  (:gen-class)
-  (:use [gulfstream.core]
-        [clojure.core.async :only [go thread close! <! >! alts!! put! take! timeout chan sliding-buffer dropping-buffer]]))
+(ns my.ns
+    (:use [gulfstream.core]))
 
 (def serverhandler
   (with-conn client
     (prn "client connected!")
     (send! client "welcome!")
     (with-data client data ;;logical listen loop is here, anything before is just when initially connected, after is when disconnected
-      (send! client data) ;;echo back
+      (send! client data) ;;echo back whatever client sent
       (prn "received:" data))
     (prn client "disconnected")))
 
-(def instream (chan (sliding-buffer 64)))
 (def clienthandler
   (with-conn server
-    (send! server "i'm here!")
-    (with-data server data (put! instream data))
+    (send! server "i'm here!") ;;on connect send this
+    (with-data server data (prn "received from server" data))
     (prn "client is now disconnected")))
 
 (def conn-details {:port 8080
@@ -43,16 +43,26 @@ clone this repo, run lein repl.
 	(conj conn-details {:handler clienthandler}))) ;;conj client-handler to replace server-handler functions
 (send! client-conn "woot")
 ;(stop-client client-conn) ;;shuts client down and disconnects socket
+
+
+;;websocket client connect variation
+;; not specifying port assumes port 80, specifying uri with ws:// protocol and optional route will start a websocket client, alternatively specify regular ip/domain with {:ws? true}
+(def ws-conn-details {:host "ws://echo.websocket.org/chat",:handler clienthandler})
+(def client-conn (start-client ws-conn-details))
+(send! client-conn "why hello there")
 ```
 
+### Non-Compliant
+
+This is not a fully RFC compliant spec websocket server nor client, but it's close and functional. Frame continuations, ping-pong and other op codes such as close are needed still. The handshaking could be improved with verification. However, this does use full frame sizing and proper masking.
 
 ### Future implementations
 
-I am going add an async option for both client and server using go blocks instead of threads-- though performance may stink; we'll see.
+I am considering about adding an async option for both client and server by using go blocks instead of threads-- though performance may suffer; we'll see.
 
-I am planning on include both ping-pong support as well as frame continuations for websockets, but need to consider raw tcp sockets as well for this to be homogenous-- this is a phase 2 sort of thing.
+I am planning to include both ping-pong support as well as frame continuations for websockets, but need to consider raw tcp sockets as well for this to be homogenous-- this is a phase 2 sort of thing.
 
-I am requesting pulls for a clojurescript variant of the client, ideally I'd like to make the client-handler the exact same but specify in connection details something like :cljs :true
+I am requesting pulls for a clojurescript variant of the client, ideally I'd like to make the client-handler the exact same but specify in connection details something like (conj conn-details {:cljs true}) and implement a backend set of functions that converts and builds a basic javascript websocket client. Keeping the client-handler peice is key.
 
 ## License
 
--- src/gulfstream/core.clj
+++ src/gulfstream/core.clj
@@ -33,20 +33,19 @@ returns seq if specifying max elements to pull"
     {:mask mask :data buf}))
 
 
-(defn- ws-encode [data & args]
+(defn ws-encode [data & args]
   "takes in bytes, return websocket frame (no chunking available); todo:chunk"
   (let [mres (if-not (empty? (filter #{:mask} args)) (ws-mask data) nil)
-        _ (prn "mres:" mres)
         data  (if mres (:data mres) data)
         len (count data)
         blen (if (> len 65535) 10 (if (> len 125) 4 2))
         buf (make-array Byte/TYPE (+ len blen (if mres 4 0)))
         _ (aset-byte buf 0 -127) ;;(bit-or (unchecked-byte 0x80) (unchecked-byte 0x1)
         _ (if (= 2 blen) 
-            (aset-byte buf 1 (bit-or (if mres 1 0) len))
+            (aset-byte buf 1 (unchecked-byte (bit-or (bit-shift-left (if mres 1 0) 7) len)))
             (do
               (dorun(map #(aset-byte buf %1 (unchecked-byte (bit-and (bit-shift-right len (*(- %2 2) 8)) 255))) (range 2 blen) (into ()(range 2 blen))))
-              (aset-byte buf 1 (if (> blen 4) 127 126))))
+              (aset-byte buf 1  (unchecked-byte (bit-or (bit-shift-left (if mres 1 0) 7) (if (> blen 4) 127 126))))))
         _ (if mres (dorun (map #(aset-byte buf (+ % blen) (nth (:mask mres) %)) (range 4))))
         _ (System/arraycopy data 0 buf (+ blen (if mres 4 0)) len)]
     buf))
@@ -54,25 +53,29 @@ returns seq if specifying max elements to pull"
 (defn send! [conn data]
   "writes to buffered output stream and flushes"
   (let [d (if (string? data) (.getBytes data "UTF8") data)
-        d (if (or (:ws? conn)(:ws conn)) (ws-encode d (if (:ws conn) :mask)) d)] ;;encode if necessary
-    (prn "sending:" (String. (if (:ws conn) (ws-decode {:data d :size (count d)}) d) "UTF8"))
-     (doto (:bouts conn)
-       (.write d 0 (count d))
-       (.flush))))
+        d (if (:ws? conn) (ws-encode d (if-not (:serversocket conn) :mask)) d)] ;;encode if necessary
+    (doto (:bouts conn)
+      (.write d 0 (count d))
+      (.flush))))
+
 
 
 ;;websocket stuff- todo: move to another file?
 
-(defn- ws-decode [frame]
+(defn ws-decode [frame & args]
   "decodes websocket frame"
-  (let [data (:data frame)
-        dlen (bit-and (second data) 127)
+  (let [;mask? (if-not (empty? (filter #{:mask} args)) true)
+        data (:data frame)
+        dlen (bit-and (second data) 127) ;0x7f/127
+        mask? (= 1(bit-and(bit-shift-right (second data) 7) 1)) ;;has a mask?
         mstart (if (== dlen 127) 10 (if (== dlen 126) 4 2))
-        mask (drop mstart (take (+ mstart 4) data))
-        msg (make-array Byte/TYPE (- (:size frame) (+ mstart 4)))]
-    (loop [i (+ mstart 4), j 0]
-      (aset-byte msg j (byte (bit-xor (nth data i) (nth mask (mod j 4)))))
-      (if (< i (dec(:size frame))) (recur (inc i) (inc j))))
+        mask (if mask? (drop mstart (take (+ mstart 4) data)))
+        msg (make-array Byte/TYPE (- (:size frame) (+ mstart (if mask? 4 0))))]
+    (if mask? 
+      (loop [i (+ mstart 4), j 0]
+        (aset-byte msg j (byte (bit-xor (nth data i) (nth mask (mod j 4)))))
+        (if (< i (dec(:size frame))) (recur (inc i) (inc j))))
+      (System/arraycopy data mstart msg 0 (- (:size frame) mstart)))
     msg))
 
 (defn- get-ws-key [req]
@@ -92,10 +95,23 @@ returns seq if specifying max elements to pull"
        ;"Sec-WebSocket-Protocol: chat\r\n" ;;todo: provide subprotocol support for non-ws sockets as well
        "\r\n")))
 
+(defn- ws-new-handshake [host port]
+  (str "GET / HTTP/1.1\r\n"
+       "Host: " host ":" port "\r\n"
+       "Upgrade: websocket\r\nConnection: Upgrade\r\n"
+       "Origin: gulfstream/clojure\r\n"
+       "Pragma: no-cache\r\nCache-Control: no-cache\r\n"
+       "Sec-WebSocket-Key: " (String.(clojure.data.codec.base64/encode (byte-array(map #(byte %)(take 16(repeatedly #(unchecked-byte(rand 255))))))) "UTF8")  "\r\n"
+       "Sec-WebSocket-Version: 13\r\n"
+      ; "Sec-WebSocket-Extensions: x-webkit-deflate-frame\r\n"
+      ; "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36\r\n"
+       "\r\n"))
+
+
+
 (defn- req-ws! [req conn]
   (if (.startsWith req "GET") ;;websocket upgrade?
-    (do(put! ds (str "websocket?!" req))
-      (send! conn (ws-handshake req))
+    (do(send! conn (ws-handshake req))
        true)
     false))
 ;;end websocket stuff
@@ -118,15 +134,13 @@ returns seq if specifying max elements to pull"
 
 (defn listen! [conn fun]
   "listens to clients instream socket"
-  (loop [data (if-not (:ws? conn)(:frame conn))]
+  (loop [data (if-not (:ws? conn) (:frame conn))]
     (when(> (or (:size data)0) -1)
-      (if-let [data (if data(if (or (:ws conn)(:ws? conn)) (String. (ws-decode data) "UTF8") (str-chunk data)))]
+      (if-let [data (if data (if (:ws? conn) (String. (ws-decode data (if (:serversocket conn) :mask)) "UTF8") (str-chunk data)))]
         ;; todo: handle onclose op codes for websockets
         (try(fun data)
             (catch Exception e (put! ds {:listen-error (.getMessage e)}))))
-
       (recur (get-chunk conn (:data(:frame conn)))))))
-
 
 
 (defmacro with-data [c v & body] 
@@ -161,19 +175,19 @@ returns seq if specifying max elements to pull"
           ;;handle if client is connecting to websocket server
           (let [conn (conj conn {:frame {:size 0 :data (make-array Byte/TYPE 4096)}})]
             (if (:ws? conn) 
-              (do(prn "connecting via websockets") 
-               conn)
-              conn)))]
-    (if-not (:serversocket conn)(prn conn))
+              (do
+                 (send! (conj conn {:ws? nil}) (ws-new-handshake (:host conn) (:port conn)))
+                 (get-chunk conn (:data(:frame conn))) ;;wait and accept handshake, throw it away and start handler down below; todo: verify handshake sec key?
+               ))
+              conn))]
 
     (try ((:handler conn) conn)
          (catch Exception e (put! ds (str (if (:serversocket conn) "server-side ") "handle error:" (.getMessage e)))))
     (close-socket (:socket conn))))
 
 
-
 (defn- await-clients [server]
-  "spins off 1 thread to accept new clients on, spins off go blocks for client handling, merges client-map with server-map to supply serversocket and handler info"
+  "spins off 1 thread to accept new clients on, spins off threads for client handling, merges client-map with server-map to supply serversocket and handler info"
   (let [server (conj server {:host (.getInetAddress (:serversocket server))})
         server (conj server 
                      {:thread (thread (loop [] 
@@ -195,7 +209,14 @@ returns seq if specifying max elements to pull"
 
 
 (defn start-client [server]
-  (let [client (conj server (build-client (Socket. (:host server) (:port server))))
+  (let [server (conj server (if-not (:port server) {:port 80}))
+        server (conj server (if (.startsWith (:host server) "ws://") 
+                              {:ws? true
+                               :host (second(clojure.string/split (:host server) #"ws://"))}))
+        server (conj server (let [h (clojure.string/split (:host server) #"/")]
+                              {:host (first h)
+                               :route (second h)}))
+        client (conj server (build-client (Socket. (:host server) (:port server))))
         client (conj client {:thread (thread(handle-conn (conj client server)))})]
     client))
 (defn stop-client [client]
